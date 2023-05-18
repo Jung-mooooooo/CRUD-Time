0bdfb7e28ff7bca981e55dfbd4dfd03e4c8865f4
Jung-mooooooo <126451648+Jung-mooooooo@users.noreply.github.com>
2023-05-18 11:54:18 +0900

    로그인 작업 80%완

---
+++ src/main/java/com/crud/btt/member/entity/MemberAuth.java
@@ -0,0 +1,44 @@
+package com.crud.btt.member.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Getter
+@Setter
+@ToString
+@Entity
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Table(name="member_auth")
+public class MemberAuth {
+
+    @Id
+    @SequenceGenerator(name = "SEQ_MEMBER_AUTH",
+            sequenceName = "SEQ_MEMBER_AUTH",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_MEMBER_AUTH")
+    private Long userAuthNo;
+
+    @Column(name = "user_code")
+    private Long userCode;
+
+    @Column(length = 50)
+    private String auth;
+
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
+//    @CreationTimestamp
+//    private LocalDateTime regDate;
+//
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
+//    @UpdateTimestamp
+//    private LocalDateTime updDate;
+
+}
